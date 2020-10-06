8571bd2d4998a1fba98c30d93990bf75a36a1065
anabellchan <anbellchan@gmail.com>
2020-10-06 16:37:35 -0700

    Add jquery to client app

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
 
 class App extends React.Component {
 
@@ -20,17 +21,22 @@ class App extends React.Component {
 
   componentDidMount() {
     // API call to product service
+    $.ajax({
+      url: '/product',
+      success: (data) => {
+        console.log(data);
+      }
+    });
     // set state based on API response
   }
 
+  render() {
+    return (
+      <h1>Hello!</h1>
+    );
+  }
 
 }
 
-const ProductApp = () => {
-  console.log('here');
-  return (
-    <h1>Hello!</h1>
-  );
-};
 
-ReactDOM.render(<ProductApp />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
