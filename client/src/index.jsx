760a9ddc8957d22f6c42360ce2adba01a48ed60a
anabellchan <anbellchan@gmail.com>
2020-10-06 20:56:54 -0700

    First draft on product service component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,42 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+import styled from 'styled-components';
+import GenderCategory from './components/GenderCategory.jsx';
+import Name from './components/Name.jsx';
+import Price from './components/Price.jsx';
+import Styles from './components/Styles.jsx';
+
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      currentProduct: {
-        productId: '',
-        name: '',
-        gender: '',
-        category: '',
-        price: '',
-        styles: [],
-        options: ''
-      }
+      productId: '',
+      name: '',
+      gender: '',
+      category: '',
+      style: {},
+      styles: [],
     };
   }
 
   componentDidMount() {
-    // API call to product service
+    console.log('fetching...');
     $.ajax({
-      url: '/product',
+      url: '/products/all/1',
       success: (data) => {
-        console.log(data);
+        console.log('server', data);
+        let d = data[0];
+        this.setState({
+          productId: d.productId,
+          name: d.name,
+          gender: d.gender === 'male' ? 'Men\'s' : 'Women\'s',
+          category: d.category,
+          style: d.styles[0],
+          styles: d.styles,
+        });
+        console.log('state', this.state);
+      },
+      error: (err) => {
+        console.error(err, err.stack);
       }
     });
-    // set state based on API response
   }
 
   render() {
     return (
-      <h1>Hello!</h1>
+      <div class="product_need_to_know">
+        <GenderCategory gender = {this.state.gender} category = {this.state.category} />
+        <Price style = {this.state.style} />
+        <Name name = {this.state.name} />
+        <Styles styles = {this.state.styles} />
+      </div>
     );
   }
-
 }
-
 
 ReactDOM.render(<App />, document.getElementById('app'));
