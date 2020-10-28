b186192c576abc008cecfe80393eebb0fd99ca41
anabellchan <anbellchan@gmail.com>
2020-10-27 21:08:10 -0700

    Add fallback data

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -63,6 +63,47 @@ class ProductModule extends React.Component {
           style: currentStyle,
           styles: stylesUpdated
         });
+      }).fail(() => {
+        console.log('handling failed request');
+        let photos = [
+          {
+            'product_id': 1,
+            'style_id': '001',
+            'main_photo': {
+              'thumbnail_url': 'https://ultimate-nike.s3.us-west-1.amazonaws.com/photos/main/thumbnail/1-001.jpg',
+              'regular_url': 'https://ultimate-nike.s3.us-west-1.amazonaws.com/photos/main/regular/1-001.jpg'
+            }
+          },
+          {
+            'product_id': 1,
+            'style_id': '002',
+            'main_photo': {
+              'thumbnail_url': 'https://ultimate-nike.s3.us-west-1.amazonaws.com/photos/main/thumbnail/1-002.jpg',
+              'regular_url': 'https://ultimate-nike.s3.us-west-1.amazonaws.com/photos/main/regular/1-002.jpg'
+            }
+          },
+          {
+            'product_id': 1,
+            'style_id': '003',
+            'main_photo': {
+              'thumbnail_url': 'https://ultimate-nike.s3.us-west-1.amazonaws.com/photos/main/thumbnail/1-003.jpg',
+              'regular_url': 'https://ultimate-nike.s3.us-west-1.amazonaws.com/photos/main/regular/1-003.jpg'
+            }
+          }
+        ];
+        let stylesUpdated = this.setThumbnails(data.styles, photos);
+        let currentStyle = stylesUpdated.filter(s => '00' + this.state.styleId === s.styleId)[0];
+        this.setState({
+          productId: data.productId,
+          details: {
+            name: data.name,
+            gender: data.gender === 'male' ? 'Men\'s' : 'Women\'s',
+            category: data.category,
+            price: currentStyle.price,
+          },
+          style: currentStyle,
+          styles: stylesUpdated
+        });
       });
     });
   }
