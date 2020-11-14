f15ffb14576085cfd76afd125c2732a887661628
Mark <mark.scrudder@gmail.com>
2020-11-14 14:41:38 -0800

    Rewrote webpack and some scripts.

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,18 @@
-const path = require('path');
-const { env } = require('process');
-const SRC_DIR = path.join(__dirname, './client/src');
-const DEST_DIR = path.join(__dirname, './client/dist');
-const webpack = require('webpack');
-const dotenv = require('dotenv');
-
-let envKeys = null;
-let variables = () => {
-  const env = dotenv.config().parsed;
-
-  envKeys = Object.keys(env).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(env[next]);
-    return prev;
-  }, {});
-
-};
-variables();
-console.log(envKeys);
-
 module.exports = {
-  node: {
-    fs: 'empty'
-  },
-  entry: `${SRC_DIR}/index.jsx`,
+  entry: `./client/src/index.jsx`,
   output: {
-    path: DEST_DIR,
-    filename: 'product-bundle.js'
+    filename: './product-bundle.js'
   },
   module: {
     rules: [
       {
-        test: /\.jsx?/,
-        include: SRC_DIR,
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react', '@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-nullish-coalescing-operator', '@babel/plugin-proposal-optional-chaining']
-          }
+        query: {
+          presets: ['@babel/preset-react', '@babel/preset-env']
         }
       }
     ]
-  },
-  plugins: [
-    new webpack.DefinePlugin(envKeys),
-  ]
-
+  }
 };
